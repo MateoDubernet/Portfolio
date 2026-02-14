b1731d6d445f890177f80a5f1971abead2f349a0
MateoDubernet <107269857+MateoDubernet@users.noreply.github.com>
2026-02-14 17:03:40 +0000

    Ajout d'une vérification pour définir le chemin par défaut dans handleLocation

--- router.js
+++ router.js
@@ -9,6 +9,7 @@ const routes = {
 
 const handleLocation = async () => {
     let path = window.location.hash.replace("#", "");
+    if (path === "") path = "/";
 
     const route = routes[path] || routes["/"];
     const html = await fetch(route).then((data) => data.text());
